036bb5c78b0c51328215d73423532a155579ee15
hang phan <38292743+hangphan91@users.noreply.github.com>
2022-03-12 01:58:32 -0600

    Add files via upload

---
+++ NailTechHelper.BeautySalonBusiness.Web-Api/Controllers/BeautySalonBusinessController.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using NailTechhelper.BusinessManager;
+using NailTechHelper.BusinessManager.Managers;
+using NailTechHelper.BusinessManager.Models;
+using System.Web.Http;
+using HttpGetAttribute = Microsoft.AspNetCore.Mvc.HttpGetAttribute;
+using HttpPostAttribute = Microsoft.AspNetCore.Mvc.HttpPostAttribute;
+
+namespace NailTechHelper.BeautySalonBusiness.Web_Api.Controllers
+{
+    [Route("BeautySalonBusiness/api")]
+    public class BeautySalonBusinessController : ControllerBase
+    {
+
+        [HttpGet("business/byId")]
+        public ActionResult GetBusinessInfoByYelpId(string yelpId)
+        {
+            var mgr = new SalonBusinessManager();
+           var businessInfo = mgr.GetBeautySalonBusinessByYelpId(yelpId);
+
+            return Ok() ;
+            
+        }
+
+        [HttpPost("add/businesses")]
+        public ActionResult AddBusinesses(BeautySalonBusinessCollection collection)
+        {
+            var mgr = new SalonBusinessManager();
+            mgr.AddBusinesses(collection);
+
+            return Ok();
+
+        }
+
+
+    }
+
+   
+}
---
+++ NailTechHelper.BeautySalonBusiness.Web-Api/Controllers/YelpFusionController.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using NailTechHelper.BusinessManager.Managers;
+using NailTechHelper.BusinessManager.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace NailTechHelper.BeautySalonBusiness.Web_Api.Controllers
+{
+    [Route("YelpFusion")]
+    public class YelpFusionController : ControllerBase
+    {
+        [HttpGet("api/YelpFusion/business/search")]
+        public async Task<ActionResult> GetBusinessInfoFromSearchBy(string term = "nail salon",
+            string location = "Fort Smith Arkansas", int limit = 50)
+        {
+            var mgr = new YelpFusionManager();
+            var businessInfos = await mgr.GetBusinessInfoFromSearchBy(term, location, limit);
+
+            var businessController = new BeautySalonBusinessController();
+            businessController.AddBusinesses(businessInfos);
+
+            return Ok();
+        }
+    }
+}
